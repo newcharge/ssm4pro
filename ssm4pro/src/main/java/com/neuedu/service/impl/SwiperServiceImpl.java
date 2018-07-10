56ee3ae16b238f1516f8519684bfc92187e77a8a
newcharge <459691746@qq.com>
2018-07-10 11:06:31 +0800

    temp
    可忽略的swiper更改

--- ssm4pro/src/main/java/com/neuedu/service/impl/SwiperServiceImpl.java
+++ ssm4pro/src/main/java/com/neuedu/service/impl/SwiperServiceImpl.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.neuedu.dao.SwiperDao;
 import com.neuedu.po.Swiper;
 import com.neuedu.service.SwiperService;
-import com.neuedu.vo.VInputSwiper;
 
 @Service
 public class SwiperServiceImpl implements SwiperService {
@@ -16,15 +16,11 @@ public class SwiperServiceImpl implements SwiperService {
 	@Autowired
 	SwiperDao swiperDao;
 	
+	@Transactional
 	@Override
-	public List<String> showSwiperByCategory(VInputSwiper vis) {
+	public List<String> showSwiperByCategory(Swiper swiper) {
 		// TODO Auto-generated method stub
-		List<String> list;
-		Swiper swiper = new Swiper();
-		swiper.setQid(vis.getQid());
-		swiper.setCategory(vis.getCategory());
-		list = swiperDao.findAllByEle(swiper);
-		return list;
+		return swiperDao.findAllByEle(swiper);
 	}
 
 }
