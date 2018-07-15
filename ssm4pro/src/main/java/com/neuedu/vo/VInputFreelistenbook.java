68ca0339fec5409a0c4168aeb8125d3e22de5ec5
zerozaki-z <494268006@qq.com>
2018-07-16 06:31:09 +0800

    后台FreelistenbookVO
    aa

--- ssm4pro/src/main/java/com/neuedu/vo/VInputFreelistenbook.java
+++ ssm4pro/src/main/java/com/neuedu/vo/VInputFreelistenbook.java
@@ -3,15 +3,11 @@ package com.neuedu.vo;
 public class VInputFreelistenbook 
 {
 	private Integer id;
-	private int fid;
 	private int qid;
-	private String username;
-	private String tel;
-	private String booktime;
+	private String status;
 	private String starttime;
 	private String endtime;
-	private String status;
-	private String comment;
+	
 	
 	public Integer getId() {
 		return id;
@@ -19,35 +15,17 @@ public class VInputFreelistenbook
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public int getFid() {
-		return fid;
-	}
-	public void setFid(int fid) {
-		this.fid = fid;
-	}
 	public int getQid() {
 		return qid;
 	}
 	public void setQid(int qid) {
 		this.qid = qid;
 	}
-	public String getUsername() {
-		return username;
+	public String getStatus() {
+		return status;
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getBooktime() {
-		return booktime;
-	}
-	public void setBooktime(String booktime) {
-		this.booktime = booktime;
+	public void setStatus(String status) {
+		this.status = status;
 	}
 	public String getStarttime() {
 		return starttime;
@@ -61,17 +39,7 @@ public class VInputFreelistenbook
 	public void setEndtime(String endtime) {
 		this.endtime = endtime;
 	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+	
+
 	
 }
--- ssm4pro/src/main/java/com/neuedu/vo/VOutputFreelistenbook.java
+++ ssm4pro/src/main/java/com/neuedu/vo/VOutputFreelistenbook.java
@@ -2,18 +2,25 @@ package com.neuedu.vo;
 
 public class VOutputFreelistenbook 
 {
-	private int fid;
+	private int id;
+	private String title;
 	private String username;
 	private String tel;
-	private String booktime;
 	private String status;
+	private String booktime;
 	private String comment;
-	
-	public int getFid() {
-		return fid;
+
+	public int getId() {
+		return id;
 	}
-	public void setFid(int fid) {
-		this.fid = fid;
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	public String getUsername() {
 		return username;
@@ -27,17 +34,17 @@ public class VOutputFreelistenbook
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
-	public String getBooktime() {
-		return booktime;
-	}
-	public void setBooktime(String booktime) {
-		this.booktime = booktime;
-	}
 	public String getStatus() {
 		return status;
 	}
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	public String getBooktime() {
+		return booktime;
+	}
+	public void setBooktime(String booktime) {
+		this.booktime = booktime;
 	}
 	public String getComment() {
 		return comment;
