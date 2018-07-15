4be069592bd8f18cf03cf74549c33e7a2c2354f8
newcharge <459691746@qq.com>
2018-07-15 22:18:01 +0800

    更新build.gradle文件（重要）
    添加分页插件支持

---
+++ ssm4pro/src/main/java/com/neuedu/web/TeacherHandler.java
@@ -0,0 +1,35 @@
+package com.neuedu.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.neuedu.po.Teacher;
+import com.neuedu.service.TeacherService;
+
+@Controller
+public class TeacherHandler {
+	
+	private static final int pageSize = 2;
+	
+	@Autowired
+	TeacherService teacherService;
+	
+	@RequestMapping(value = "teacher/showAll.action")
+	@ResponseBody
+	public List<Teacher> showTeacherByQid(int qid) throws Exception {
+		return teacherService.showTeacher(qid);
+	}
+	
+	@RequestMapping(value = "teacher/showAllByPage.action")
+	@ResponseBody
+	public PageInfo<Teacher> showTeacherPageByQid(int qid, int pageNum) throws Exception {
+		PageHelper.startPage(pageNum, pageSize);
+		return PageInfo.of(teacherService.showTeacher(qid));
+	}
+}
