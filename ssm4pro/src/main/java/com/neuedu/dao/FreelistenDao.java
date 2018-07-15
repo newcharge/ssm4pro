785ac3555c24aa28c474a756ad4ad6ab245b0f11
zerozaki-z <494268006@qq.com>
2018-07-16 06:39:20 +0800

    后台FreelistenbookDao+FreelistenDao
    aa

--- ssm4pro/src/main/java/com/neuedu/dao/FreelistenDao.java
+++ ssm4pro/src/main/java/com/neuedu/dao/FreelistenDao.java
@@ -10,7 +10,8 @@ public interface FreelistenDao
 	public boolean editFreelisten(Freelisten freelisten) throws Exception;
 	public boolean deleteFreelisten(int id) throws Exception;
 	public List<Freelisten> showFreelisten(int qid) throws Exception;
-	public List<Freelisten> showFreelistenByBranchId(Freelisten freelisten) throws Exception;
+	public Freelisten showFreelistenById(int id) throws Exception;
+	public List<Freelisten> showFreelistenByBranchId(int branchid) throws Exception;
 	
 	
 }
--- ssm4pro/src/main/java/com/neuedu/dao/FreelistenbookDao.java
+++ ssm4pro/src/main/java/com/neuedu/dao/FreelistenbookDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.neuedu.po.Freelistenbook;
 import com.neuedu.vo.VInputFreelistenbook;
+import com.neuedu.vo.VOutputFreelistenbook;
 
 public interface FreelistenbookDao 
 {
@@ -12,7 +13,7 @@ public interface FreelistenbookDao
 	//改变状态
 	public boolean changeFreelistenbookState(Freelistenbook freelistenbook) throws Exception;
 	//全部查询
-	public List<Freelistenbook> showFreelistenbook(int qid) throws Exception;
+	public List<VOutputFreelistenbook> showFreelistenbook(int qid) throws Exception;
 	//三种带参查询
-	public List<Freelistenbook> showFreelistenbookByOthers(VInputFreelistenbook vif) throws Exception;
+	public List<VOutputFreelistenbook> showFreelistenbookByOthers(VInputFreelistenbook vif) throws Exception;
 }
