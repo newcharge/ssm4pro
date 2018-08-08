0d06b60623222c1e932b27a80f8fcbfe92db16f1
newcharge <459691746@qq.com>
2018-08-08 09:19:05 +0800

    refresh

--- ssm4pro/src/test/java/com/neuedu/service/impl/EnterpriseServiceImplTest.java
+++ ssm4pro/src/test/java/com/neuedu/service/impl/EnterpriseServiceImplTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.neuedu.service.EnterpriseService;
+import com.neuedu.vo.VInputEnterprise;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
@@ -18,6 +19,17 @@ public class EnterpriseServiceImplTest {
 	@Test
 	public void testShowEnterprise() throws Exception {
 		System.out.println(enterpriseService.showEnterprise(1));
+	}
+	
+	@Test
+	public void testeditEnterprise() throws Exception {
+		VInputEnterprise vie=new VInputEnterprise();
+		vie.setQid(2);
+		vie.setVideopath("aaa");
+		vie.setIntroduction("bbb");
+		vie.setJczs("aaa");
+		vie.setName("zzz");
+		System.out.println(enterpriseService.editEnterprise(vie));
 	}
 
 }
